bacce8c7dce2026d81564eb8d54d6e5dcc6c704d
jhackett1 <joshua.hackett93@gmail.com>
2018-07-03 11:42:49 +0100

    Added accordion layout to results page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,28 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+
+  // Accordian on results page
+  let accordia = document.querySelectorAll('.app-task-list .app-task-list__item')
+  accordia.forEach((accordion)=>{
+
+
+    accordion.querySelector('button').addEventListener('click', (e)=>{
+      let description = accordion.querySelector('.description');
+      let label = accordion.querySelector('.show-label');
+      // Open/close description
+      if (description.classList.contains('open')) {
+        description.classList.remove('open')
+      } else {
+        description.classList.add('open')
+      }
+      // Change label text
+      if (label.innerHTML === 'Show details') {
+        label.innerHTML = 'Hide details'
+      } else {
+        label.innerHTML = 'Show details'
+      }
+    })
+  })
 })
--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Extra help you can get',
+  serviceName: 'Check what support you can get',
 
   // Default port that prototype runs on
   port: '3000',
